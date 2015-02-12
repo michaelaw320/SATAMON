c926b0a19dc39bbd1274ed43e20e70b00de32703
Imballinst <lucent_rays@yahoo.com>
2015-02-12 18:06:30 +0700

    front-end commit v2.2

--- frontend/index.php
+++ frontend/index.php
@@ -7,50 +7,14 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	
-	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/bootstrap-select.css" rel="stylesheet">
-	<link href="css/screen.css" rel="stylesheet">
   
-	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/scripts.js"></script>
 	<script type="text/javascript" src="js/bootstrap-select.js"></script>
-	<script type="text/javascript" src="js/jquery.validate.js"></script>
-	<script>
-	$.validator.setDefaults({
-		submitHandler: function() {
-			alert("submitted!");
-		}
-	});
-
-	$().ready(function() {
-		// validate the comment form when it is submitted
-		$("#aduan").validate();
-
-		// propose username by combining first- and lastname
-		$("#username").focus(function() {
-			var firstname = $("#firstname").val();
-			var lastname = $("#lastname").val();
-			if (firstname && lastname && !this.value) {
-				this.value = firstname + "." + lastname;
-			}
-		});
-
-		//code to hide topic selection, disable for demo
-		var newsletter = $("#newsletter");
-		// newsletter topics are optional, hide at first
-		var inital = newsletter.is(":checked");
-		var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
-		var topicInputs = topics.find("input").attr("disabled", !inital);
-		// show when newsletter is checked
-		newsletter.click(function() {
-			topics[this.checked ? "removeClass" : "addClass"]("gray");
-			topicInputs.attr("disabled", !this.checked);
-		});
-	});
-	</script>
 	
 	<title>Sistem Aduan Taman Online Kota Bandung - Laporkan!</title>
 </head>
