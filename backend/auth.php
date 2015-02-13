e4128b482cdc7fbae675e5126812c8595b9aed70
michaelaw320 <michaelaw320@gmail.com>
2015-02-13 22:39:06 +0700

    Auth.php module done Documentation added

--- backend/auth.php
+++ backend/auth.php
@@ -1,15 +1,37 @@
 <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
+/**
+ *	Authentication Module
+ *	Input: 
+ *		Method: POST
+ *		Content: username, password
+ *	Output:
+ *		JSON
+ *		Content: {"LoginStatus":"SUCCESS/FAIL"}
+ *	Process:
+ *		Compares the username and password sent by client with hardcoded credentials
+ *		If auth successful, start session, return JSON with success flag
+ *		Else return JSON with fail flag
+*/
 
+/* Hardcoded Credentials */
 $admin_username = "admin";
 $admin_password = "admin";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
 
 if($username == $admin_username && $password == $admin_password) {
 	//stub for success login
 	session_start();
+	$data = array('LoginStatus'=>"SUCCESS");
+	$send = json_encode($data);
+	header('Content-Type: application/json');
+	echo $send;
 } else {
 	//stub for false login
-	echo 'Tai';
+	$data = array('LoginStatus'=>"FAIL");
+	$send = json_encode($data);
+	header('Content-Type: application/json');
+	echo $send;
 }
 ?>
--- backend/logout.php
+++ backend/logout.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ *	Logout Module
+ *	Input:
+ *		N/A
+ *	Output:
+ *		N/A
+ *	Process:
+ *		Unset and destroy the session
+ */
+
 session_unset();
 session_destroy();
 ?>
