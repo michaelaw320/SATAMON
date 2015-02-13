4e26dbb175f36aa65d4230c64928c717a4de18d8
imballinst <lucent_rays@yahoo.com>
2015-02-13 14:06:29 +0700

    front-end commit v2.3
    tambahin logo satamon, edit css

--- frontend/index.php
+++ frontend/index.php
@@ -8,7 +8,7 @@
 	<meta name="author" content="">
 
 	<link href="css/style.css" rel="stylesheet">
-	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
 	<link href="css/bootstrap-select.css" rel="stylesheet">
   
 	<script type="text/javascript" src="js/jquery.min.js"></script>
@@ -23,56 +23,9 @@
 <div class="container">
 	<div class="row clearfix">
 		<div class="col-md-12 column">
-			<h3 class="text-center">
-				Sistem Aduan Taman Online Kota Bandung
-			</h3>
-			<!--
-			<div class="carousel slide" id="carousel-621411">
-				<ol class="carousel-indicators">
-					<li data-slide-to="0" data-target="#carousel-621411">
-					</li>
-					<li data-slide-to="1" data-target="#carousel-621411" class="active">
-					</li>
-					<li data-slide-to="2" data-target="#carousel-621411">
-					</li>
-				</ol>
-				<div class="carousel-inner">
-					<div class="item">
-						<img alt="" src="http://lorempixel.com/1600/500/sports/1">
-						<div class="carousel-caption">
-							<h4>
-								Cara mudah membantu Pemkot Bandung #1
-							</h4>
-							<p>
-								Isilah form dengan data-data yang disediakan.
-							</p>
-						</div>
-					</div>
-					<div class="item active">
-						<img alt="" src="http://lorempixel.com/1600/500/sports/2">
-						<div class="carousel-caption">
-							<h4>
-								Cara mudah membantu Pemkot Bandung #2
-							</h4>
-							<p>
-								Tekan tombol submit untuk mengirimkan pengaduan.
-							</p>
-						</div>
-					</div>
-					<div class="item">
-						<img alt="" src="http://lorempixel.com/1600/500/sports/3">
-						<div class="carousel-caption">
-							<h4>
-								Cara mudah membantu Pemkot Bandung #3
-							</h4>
-							<p>
-								Terima kasih! Anda telah membantu Pemkot Kota Bandung dalam menjaga kenyamanan bersama!
-							</p>
-						</div>
-					</div>
-				</div> <a class="left carousel-control" href="#carousel-621411" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-621411" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-			</div>
-			-->
+			<h3 class="text-center"><b>
+				<img src="/SATAMON/frontend/img/satamon.png" height="360" width="360"></img>
+			</b></h3>
 			<form role="form" class="cmxform" id="aduan">
 				<div class="form-group">
 					 <label for="inputEmail">Alamat E-mail</label><input class="form-control" id="inputEmail1" type="email">
