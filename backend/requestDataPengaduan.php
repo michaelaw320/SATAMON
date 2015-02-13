b355dbf4eaed7ed9e2c91a8a76aadbdfb8ef8487
michaelaw320 <michaelaw320@gmail.com>
2015-02-13 22:58:43 +0700

    Request data pengaduan stub added Request Laporan pengaduan stub added

---
+++ backend/requestDataPengaduan.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *	Request Data Pengaduan
+ *	Input:
+ *		TBD
+ *	Output:
+ *		JSON
+ *		Content: TBD
+ *	Process:
+ *		Query database untuk laporan dan foto sesuai dengan ketentuan
+ *		return hasil retrieval kepada client
+ */
+
+?>
---
+++ backend/requestLaporanPengaduan.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *	Request Laporan Pengaduan
+ *	Input:
+ *		TBD
+ *	Output:
+ *		JSON
+ *		Content: TBD
+ *	Process:
+ *		TBD, bingung juga ini mau ngapain sebenernya disini
+ */
+
+?>
