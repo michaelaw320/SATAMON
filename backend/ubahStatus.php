0baa57ce94ba8a5f0658201f565fded73ef94732
michaelaw320 <michaelaw320@gmail.com>
2015-02-16 15:28:40 +0700

    documentation updated

--- backend/ubahStatus.php
+++ backend/ubahStatus.php
@@ -3,13 +3,16 @@
  *	Ubah Status Pengaduan
  *	Input:
  *		Method: POST
- *		Content: JSON List of selected id to be updated and its value {id:value} pair
+ *		Content: JSON List of selected id to be updated and its value {id:value, id:value, id:value, ...} pair
+ *					id -> id laporan
+ *					value -> diterima, diproses, atau selesai
  *	Output:
  *		JSON
  *		Not Authenticated JSON: {'LoginStatus':'NOT_AUTHENTICATED'}
  *		Content: {'result' : 'DONE'}
  *	Process:
- *		TBD, bingung juga ini mau ngapain sebenernya disini
+ *		Ubah database menurut permintaan frontend
+ *		Lalu kirim email individual kepada pelapor sesuai yang tersimpan di basis data
  */
 
 require 'MYSQL_CREDENTIALS.php';
@@ -32,7 +35,12 @@ if(session_status() == PHP_SESSION_NONE) {
 		$json = file_get_contents('php://input');
 		$data = json_decode($json,true);
 		
-		foreach($data as $key => $value) {		
+		foreach($data as $key => $value) {
+			//$key as id laporan
+			//$value itu status laporan
+			//TODO email ke masyarakat yang status pengaduannya diubah, tony modules go here
+			// algoritma kasar mailer, query ke basisdata ambil email yang sesuai dengan id nya	
+			// hati hati penanganan email NULL	
 			$value = "'". $value. "'";
 			$sql = "UPDATE `pengaduan` SET status_pengaduan=$value WHERE id_pengaduan=$key";
 			$update = mysqli_query($conn, $sql);
